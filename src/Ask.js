3f84dcc2111d53917d8a89bada960c6276d09d7e
Sam Vente <sam.vente@source.ag>
2022-12-22 11:57:55 +0100

    ask submission form works now

--- src/Ask.js
+++ src/Ask.js
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useFetch from "./useFetch";
 
 const Ask = () => {
   const [question, setQuestion] = useState("");
+  const {
+    data: responses,
+    isLoading,
+    error,
+  } = useFetch("http://localhost:8000/responses");
 
   const handleSubmit = (ev) => {
     ev.preventDefault();
+    const body = JSON.stringify({ question });
     fetch("http://localhost:8000/ask", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(question),
+      body: body,
     }).then(() => console.log("question submited"));
+
+    const randomResponseIndex = Math.ceil(Math.random() * responses.length);
+    const response = responses[randomResponseIndex];
+    alert("Q: " + question + "\n" + "A: " + response);
   };
 
   return (
@@ -30,10 +41,6 @@ const Ask = () => {
         <br />
         <button>What would my PM say?</button>
       </form>
-      <br />
-      Your question repeated:
-      <br />
-      {question}
     </main>
   );
 };
